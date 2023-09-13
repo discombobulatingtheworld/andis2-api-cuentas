01608ff2f6f6c7b7b2d38339d61197b7434485f7
Santiago De Olivera <santiagodeolivera@gmail.com>
2023-09-13 20:33:17 -0300

    Create VS project

---
+++ andis2-api-cuentas/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace andis2_api_cuentas.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
