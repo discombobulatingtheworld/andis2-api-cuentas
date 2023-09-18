d355d967678cdf01665f4f9391e07a3a9b40b38b
jumpert <juan.perezc@correo.ucu.edu.uy>
2023-09-18 19:32:07 -0300

    controller

--- andis2-api-cuentas/Models/Account.cs
+++ andis2-api-cuentas/Models/Account.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
 
 namespace andis2_api_cuentas.Models;
 public class Account
 {
+    [Key]
     public int accountNumber { get; set; }
     public int accountBalance { get; set; }
     public string accountName { get; set; }
     public int ownerId { get; set; }
-    public List<string> permissions { get; set; }
+    public string permissions { get; set; }
 }
