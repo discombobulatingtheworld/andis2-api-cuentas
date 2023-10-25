8fc75740ccbd8b0c347403daf134e2c536dc1e1b
jumpert <juan.perezc@correo.ucu.edu.uy>
2023-10-25 20:48:31 -0300

    Concurrency implemented

--- andis2-api-cuentas/Program.cs
+++ andis2-api-cuentas/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using andis2_api_cuentas.Models;
+using Microsoft.AspNetCore.RateLimiting;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,20 @@ builder.Services.AddDbContext<AccountContext>(opt => opt.UseInMemoryDatabase("Ac
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429; // Too many request
+    options.OnRejected = async (context, token) =>
+    {
+        await context.HttpContext.Response.WriteAsync("muchas llamadas, por favor prueba mas tarde ");
+    };
+    options.AddConcurrencyLimiter(policyName: "concurrencyPolicy", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 10;
+        limiterOptions.QueueLimit = 100;
+        limiterOptions.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+    });
+});
 
 
 
@@ -25,6 +41,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+app.UseRateLimiter();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
